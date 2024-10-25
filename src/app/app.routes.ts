d27497c827815ee30b3242699d79b086d51a2e04
sinansrk <sinansrk966@gmail.com>
2024-10-25 21:58:26 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './page/login/login.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { ProductComponent } from './page/product/product.component';
+
+export const routes: Routes = [
+{
+  path:'',component: LoginComponent
+},
+{
+  path:'home',component:HomeComponent
+},
+
+
+{
+  path:'products/:id',component:ProductComponent
+}
+
+];
---
+++ src/app/ui/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardsComponent {
+
+}
